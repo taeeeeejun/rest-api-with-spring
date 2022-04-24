3699d6261ec5eb754e9c47de4a720d1fe8d727f1
박태준 <ptj@ptj.local>
2022-04-24 18:14:17 +0900

    Event domain

---
+++ src/main/java/com/example/restapiwithspring/events/EventStatus.java
@@ -0,0 +1,6 @@
+package com.example.restapiwithspring.events;
+
+public enum EventStatus {
+
+    DRAFT, PUBLISHED, BEGAN_ENROLLMENT;
+}
---
+++ src/test/java/com/example/restapiwithspring/events/EventTest.java
@@ -0,0 +1,33 @@
+package com.example.restapiwithspring.events;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class EventTest {
+
+    @Test
+    void builder() {
+        Event event = Event.builder()
+                .name("Inflearn Spring REST API")
+                .description("REST API development with Spring")
+                .build();
+        assertThat(event).isNotNull();
+    }
+
+    @Test
+    void javaBean() {
+        // Given
+        String name = "Event";
+        String description = "Spring";
+
+        // When
+        Event event = new Event();
+        event.setName(name);
+        event.setDescription(description);
+
+        // Then
+        assertThat(event.getName()).isEqualTo(name);
+        assertThat(event.getDescription()).isEqualTo(description);
+    }
+}
